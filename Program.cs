ce6f98db26ba9009ddca31839439664b9b4e9dd5
magohl <magnus.ohlsson@gmail.com>
2021-09-11 21:37:16 +0000

    Modified to .net 6

--- Program.cs
+++ Program.cs
@@ -1,26 +1,36 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http.Features;
 
-namespace WhoamiCore
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("{*url}", async context =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    context.Response.Headers.Add("Cache-Control", "no-cache");
+    await WriteRequestInfo(context);
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging=>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+app.Run();
+
+static async Task WriteRequestInfo(HttpContext context)
+{
+    //Write connection, request and system information
+    await context.Response.WriteAsync($"Hostname: {System.Net.Dns.GetHostName()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Method: {context.Request.Method}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Path: {context.Request.Path}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Scheme: {context.Request.Scheme}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Host header: {context.Request.Host}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Remote-Ip:port: {context.Connection.RemoteIpAddress.MapToIPv4().ToString()}:{context.Connection.RemotePort.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Local-Ip:port: {context.Connection.LocalIpAddress.MapToIPv4().ToString()}:{context.Connection.LocalPort.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"OS Architecture: {System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"OS Description: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Runtime identifier: {System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Framework: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Process Architecture: {System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Processor count: {System.Environment.ProcessorCount}{Environment.NewLine}");
+    await context.Response.WriteAsync($"System Version: {System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion()}{Environment.NewLine}");
+    //Write HTTP headers
+    foreach (var header in context.Request.Headers)
+    {
+        await context.Response.WriteAsync($"Request Http Header - {header.Key}: {header.Value}{Environment.NewLine}");
     }
 }
