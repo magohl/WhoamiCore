ef7931a84d12d6bd990c0df7af6e8c0299a7a0af
magnus.ohlsson@gmail.com <magnus.ohlsson@gmail.com>
2022-12-12 22:05:49 +0000

    changed to top level statements

--- Program.cs
+++ Program.cs
@@ -1,26 +1,62 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Microsoft.AspNetCore.HttpOverrides;
 
-namespace WhoamiCore
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.ForwardedHeaders = ForwardedHeaders.All;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging=>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    await RequestStopWatch(context, next);
+});
+
+app.UseForwardedHeaders();
+
+app.MapGet("{*url}", async context =>
+{
+    context.Response.Headers.Add("Cache-Control", "no-cache");
+    await WriteRequestInfo(context);
+});
+
+app.Run();
+
+/* helper methods */
+static async Task WriteRequestInfo(HttpContext context)
+{
+    //Write connection, request and system information
+    await context.Response.WriteAsync($"Hostname: {System.Net.Dns.GetHostName()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Method: {context.Request.Method}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Path: {context.Request.Path}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Scheme: {context.Request.Scheme}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Host header: {context.Request.Host}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Remote-Ip:port: {context.Connection.RemoteIpAddress?.MapToIPv4().ToString()}:{context.Connection.RemotePort.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Local-Ip:port: {context.Connection.LocalIpAddress?.MapToIPv4().ToString()}:{context.Connection.LocalPort.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"OS Architecture: {System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"OS Description: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Runtime identifier: {System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Framework: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Process Architecture: {System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString()}{Environment.NewLine}");
+    await context.Response.WriteAsync($"Processor count: {System.Environment.ProcessorCount}{Environment.NewLine}");
+    await context.Response.WriteAsync($"System Version: {System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion()}{Environment.NewLine}");
+    //Write HTTP headers
+    foreach (var header in context.Request.Headers)
+    {
+        await context.Response.WriteAsync($"Request Http Header - {header.Key}: {header.Value}{Environment.NewLine}");
     }
+}
+
+static async Task RequestStopWatch(HttpContext context, Func<Task> next)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    await next.Invoke();
+    sw.Stop();
+    await context.Response.WriteAsync($"Request pipeline roundtrip: {sw.ElapsedMilliseconds}ms{Environment.NewLine}");
 }
